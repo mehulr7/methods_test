2bbdeb7521d13e8e1c749e371b85f8620dfa891c
Mehul Ruparelia <mehul.ruparelia@itv.com>
2015-10-16 12:40:14 +0100

    added some more methods

--- methods_test.rb
+++ methods_test.rb
@@ -25,3 +25,43 @@ def say_hi_from_three_people(input, input_2, input_3)
 end
 
 say_hi_from_three_people("kiran", "cheta", "ade")
+
+def add(num1, num2)
+  puts num1 + num2
+end
+
+
+add(5,6)
+add(3,1)
+add(1,9)
+
+
+def multiply(num1, num2)
+  puts num1 * num2
+end
+
+multiply(4,6)
+multiply(3,2)
+multiply(1,2)
+
+def subtract(num1, num2)
+  puts num1 - num2
+end
+
+subtract(3,1)
+subtract(8,1)
+subtract(3,9)
+
+def farenheit(celcius)
+  puts (celcius * 2) + 30
+end
+
+farenheit(13)
+
+
+def celsius(farenheit)
+  puts (farenheit - 32) * 5 / 9
+end
+
+celsius(55)
+
